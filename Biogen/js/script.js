31c1b32f14e4e02ac29c808495c8a29ff5b61d87
Abdina <46098717+Abdina@users.noreply.github.com>
2019-01-11 11:39:34 +0600

    Add files via upload

---
+++ Biogen/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.menu').on('click', function () {
+    $('nav ul').toggleClass('invisible')
+  })
+
+  $('.go_to').click(function () {
+    var scroll_el = $(this).attr('href')
+    if ($(scroll_el).length != 0) {
+    	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800)
+    }
+    return false
+  })
+})
